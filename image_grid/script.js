db80e57249acb68b0a713b6aa005f899b8624d03
ezechiel <ezechielsums001@gmail.com>
2021-07-27 16:18:10 +0200

    All Web Free Project

---
+++ image_grid/script.js
@@ -0,0 +1,17 @@
+const boxesContainer = document.getElementById('monContainer')
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', () => boxesContainer.classList.toggle('munene'))
+
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement('div')
+      box.classList.add('box')
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+      boxesContainer.appendChild(box)
+    }
+  }
+}
+
+createBoxes()
